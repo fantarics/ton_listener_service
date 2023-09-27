778d04a94a68479899b2bf4a2445189814db97f6
BoryslavGlov <boryslav.ziubrytskyi@providers.glovoapp.com>
2023-09-27 16:30:40 +0300

    feat: readme file & fixes

--- service/languagesMap.go
+++ service/languagesMap.go
@@ -3,5 +3,5 @@ package service
 var receivedMap = map[string]string{"ru": "Вы получили {amount} {currency}", "en": "You received {amount} {currency}"}
 
 var transferredMap = map[string]string{"ru": "Bы отправили: {floatAmount} {currency} Адрес получателя: {destination}",
-	"en": "Bы отправили: {floatAmount} {currency} Адрес получателя: {destination}",
+	"en": "You transferred: {floatAmount} {currency} Receiver address: {destination}",
 }
